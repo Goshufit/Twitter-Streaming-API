98439f9c2b2717ef24caefa30eccc7d6d5d2cad3
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-08-07 23:51:07 -0400

    Add HTML file to post tweets visually create server on my computer to rune script from connect to local host direct the path of local host to the JS files add JS to HTML file to log tweets

--- server/index.js
+++ server/index.js
@@ -4,19 +4,35 @@ const path = require('path');
 const express = require('express')
 socketIo = require('socket.io')
 const needle = require('needle');
+const { isObject } = require('util');
 const config = require('dotenv').config();
+// The process object provides information about, and control over, the current Node.js process. //
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 const PORT = process.env.PORT || 3000
 
 // Creates an Express application. The express() function is a top-level function exported by the express module. //
+// Express is a node js web application framework that provides broad features for building web and mobile applications. It is used to build a single page, multipage, and hybrid web application. It's a layer built on the top of the Node js that helps manage servers and routes //
 const app = express()
+
+// The http.createServer() method turns your computer into an HTTP server.
+// The HTTP Server object can listen to ports on your computer and execute a function, a requestListener, each time a request is made. //
+// A node port exposes the service on a static port on the node IP address. NodePorts are in the 30000-32767 range by default, which means a NodePort is unlikely to match a service's intended port (for example, 8080 may be exposed as 31020). //
+// The http.createServer() method creates an HTTP Server object. //
+const server = http.createServer(app)
+const io = socketIo(server)
+
+app.get('/', (req, res) => {
+    // The res. sendFile() function basically transfers the file at the given path and it sets the Content-Type response HTTP header field based on the filename extension. //
+    // The path. resolve() method is used to resolve a sequence of path-segments to an absolute path. It works by processing the sequence of paths from right to left, prepending each of the paths until the absolute path is created. The resulting path is normalized and trailing slashes are removed as required. //
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
+})
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 // The "?" is a query in the URL after that we add tweet.field to find the public metrics for instance how many retweets or comments then we add an expansion to the query to find the authors ID who posted the tweet //
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
 
-const rules = [{ value: 'hamburger' }]
+const rules = [{ value: 'cars' }]
 
 // Get Stream Rules //
 async function getRules() {
@@ -132,45 +148,62 @@ async function deleteRules(rules) {
     return response.body
 }
 
-function streamTweets() {
+function streamTweets(socket) {
     const stream = needle.get(streamURL, {
         headers: {
             Authorization: `Bearer ${TOKEN}`
         }
     })
 
+    // In Node. js, you usually bind functions (using 'on' or other functions) to listen to events. //
     // "On" Adds the listener function to the end of the listeners array for the event named eventName. No checks are made to see if the listener has already been added. Multiple calls passing the same combination of eventName and listener will result in the listener being added, and called, multiple times. //
     // A stream is an abstract interface for working with streaming data in Node.js. The node:stream module provides an API for implementing the stream interface. //
     // Streams can be readable, writable, or both. All streams are instances of EventEmitter. //
     stream.on('data', (data) => {
         try {
             const json = JSON.parse(data)
-            console.log(json)
+            // console.log(json)
+            // In programming, a socket is an endpoint of a communication between two programs running on a network. Sockets are used to create a connection between a client program and a server program. //
+            // emit is used to trigger an event on is used to add a callback function that's going to be executed when the event is triggered //
+            // This will allow the tweet data stored as JSON data to be sent
+            socket.emit('tweet', json)
         } catch (error) {
 
         }
     })
 }
 
+// In Node. js, you usually bind functions (using 'on' or other functions) to listen to events. //
+// "On" Adds the listener function to the end of the listeners array for the event named eventName. No checks are made to see if the listener has already been added. Multiple calls passing the same combination of eventName and listener will result in the listener being added, and called, multiple times. //
+// Socket.IO is a library that enables low-latency, bidirectional and event-based communication between a client and a server. //
+// Event: 'connect' Added in: v0.1.90 Emitted when a socket connection is successfully established. See net.createConnection(). //
+io.on('connection', async () => {
+        console.log('Client connected...');
 
-(async () => {
-    let currentRules
+        let currentRules
 
-    try {
-        // Get all stream rules //
-        currentRules = await getRules()
-        // Delete all stream rules //
-        await deleteRules(currentRules)
-        // Set rules based on array above//
-        await setRules()
-    } catch (error) {
-        console.log(error)
-        // The process object provides information about, and control over, the current Node.js process. //
-        // The 'exit' event is emitted when the Node.js process is about to exit as a result of either://
-        // The process.exit() method being called explicitly;//
-        // The Node.js event loop no longer having any additional work to perform.//
-        process.exit(1)
-    }
+            try {
+                // Get all stream rules //
+                currentRules = await getRules()
+                // Delete all stream rules //
+                await deleteRules(currentRules)
+                // Set rules based on array above//
+                await setRules()
+            } catch (error) {
+                console.log(error)
+                // The process object provides information about, and control over, the current Node.js process. //
+                // The 'exit' event is emitted when the Node.js process is about to exit as a result of either://
+                // The process.exit() method being called explicitly;//
+                // The Node.js event loop no longer having any additional work to perform.//
+                process.exit(1)
+            }
+        
+            streamTweets(io)
+    })
 
-    streamTweets()
-})()
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+
+// In terminal how to run project "npm run dev" //
+
+// The npm run dev command is a generic npm command that you can find in many modern web application projects. This command is used to run the dev script defined in the project's package. json file. //
